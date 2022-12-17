d35d84426df44238af4c550de649e65d13441ffc
OJailson17 <JaylsonO17@gmail.com>
2022-12-17 05:49:32 -0300

    test: add tests to read notification

---
+++ src/app/useCases/readNotification.spec.ts
@@ -0,0 +1,34 @@
+import { makeNotification } from '@test/factories/notificationFactory';
+import { NotificationsRepositoryInMemory } from '@test/repositories/notificationsRepositoryInMemory';
+import { NotificationNotFound } from './errors/notificationNotFound';
+import { ReadNotification } from './readNotification';
+
+const notificationsRepositoryInMemory = new NotificationsRepositoryInMemory();
+
+describe('Read notification', () => {
+  it('should be able to read a notification', async () => {
+    const readNotification = new ReadNotification(
+      notificationsRepositoryInMemory,
+    );
+
+    const notification = makeNotification();
+
+    await notificationsRepositoryInMemory.create(notification);
+
+    await readNotification.execute({ notificationId: notification.id });
+
+    expect(notificationsRepositoryInMemory.notifications[0].readAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('should not be able to read a notification if it does not exist', async () => {
+    const readNotification = new ReadNotification(
+      notificationsRepositoryInMemory,
+    );
+
+    expect(() => {
+      return readNotification.execute({ notificationId: 'wrong-id' });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});

1e80ec99b767d37a6b95efdb43dbc0ba06044fb0
OJailson17 <JaylsonO17@gmail.com>
2022-12-17 05:55:23 -0300

    test: add tests to unread notification

---
+++ src/app/useCases/unreadNotification.spec.ts
@@ -0,0 +1,34 @@
+import { makeNotification } from '@test/factories/notificationFactory';
+import { NotificationsRepositoryInMemory } from '@test/repositories/notificationsRepositoryInMemory';
+import { NotificationNotFound } from './errors/notificationNotFound';
+import { UnreadNotification } from './unreadNotification';
+
+const notificationsRepositoryInMemory = new NotificationsRepositoryInMemory();
+
+describe('Unread notification', () => {
+  it('should be able to unread a notification', async () => {
+    const unreadNotification = new UnreadNotification(
+      notificationsRepositoryInMemory,
+    );
+
+    const notification = makeNotification({
+      readAt: new Date(),
+    });
+
+    await notificationsRepositoryInMemory.create(notification);
+
+    await unreadNotification.execute({ notificationId: notification.id });
+
+    expect(notificationsRepositoryInMemory.notifications[0].readAt).toBeNull();
+  });
+
+  it('should not be able to unread a notification if it does not exist', async () => {
+    const unreadNotification = new UnreadNotification(
+      notificationsRepositoryInMemory,
+    );
+
+    expect(() => {
+      return unreadNotification.execute({ notificationId: 'wrong-id' });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
